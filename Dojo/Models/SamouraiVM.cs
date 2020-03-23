9c1c7e3ca8d048115956cc5b2ad76d38a2d673d8
Xenophon <schweitzer.maxime.06@gmail.com>
2020-03-23 13:04:38 +0100

    initial commit

---
+++ Dojo/Models/SamouraiVM.cs
@@ -0,0 +1,15 @@
+﻿using BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dojo.Models
+{
+    public class SamouraiVM
+    {
+        public Samourai Samourai { get; set; }
+        public List<Arme> Armes { get; set; }
+        public int? SelectedArme { get; set; }
+    }
+}
